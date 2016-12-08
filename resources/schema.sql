af09a609a0482a22e6ec931b038df4c1eefcce4c
Christian Egli <christian.egli@sbs.ch>
2016-12-08 11:35:46 +0100

    Add comments on how to create a db and a db user

--- resources/schema.sql
+++ resources/schema.sql
@@ -1,3 +1,11 @@
+-- Create a db
+-- CREATE DATABASE hyphenation CHARACTER SET utf8 COLLATE utf8_bin;
+
+-- Create a user
+-- CREATE USER 'hyphenation'@'localhost' IDENTIFIED BY 'sekret';
+-- GRANT ALL ON hyphenation.* TO 'hyphenation'@'localhost';
+-- FLUSH PRIVILEGES;
+
 -- Type of spelling
 -- a classic reference table
 CREATE TABLE spelling (
