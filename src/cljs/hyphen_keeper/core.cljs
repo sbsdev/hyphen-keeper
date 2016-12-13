a1bfc746b8e6330ae86e1354931be5cc2e8b56dd
Christian Egli <christian.egli@sbs.ch>
2016-12-13 11:24:42 +0100

    Implement a basic client
    - initial load
    - create a new hyphenation pattern

--- src/cljs/hyphen_keeper/core.cljs
+++ src/cljs/hyphen_keeper/core.cljs
@@ -1,15 +1,105 @@
 (ns hyphen-keeper.core
-    (:require [reagent.core :as reagent :refer [atom]]
-              [reagent.session :as session]
-              [secretary.core :as secretary :include-macros true]
-              [accountant.core :as accountant]))
+  (:require [accountant.core :as accountant]
+            [ajax.core :as ajax]
+            [clojure.string :as string]
+            [reagent.core :as reagent]
+            [reagent.session :as session]
+            [secretary.core :as secretary :include-macros true]))
+
+(defn load-hyphenation-patterns!
+  [state]
+  (ajax/GET "/api/0/words"
+            :handler (fn [hyphenations] (swap! state assoc :hyphenations hyphenations))
+            :error-handler (fn [details]
+                             (.warn js/console
+                                    (str "Failed to refresh hyphenation patterns from server: " details)))
+            :response-format :json
+            :keywords? true))
+
+(defn add-hyphenation-pattern!
+  [state word hyphenation]
+  (let [pattern {:word word :hyphenation hyphenation}]
+    (ajax/POST "/api/0/words"
+               :params pattern
+               :handler (fn [] (swap! state update-in [:hyphenations] conj pattern))
+               :error-handler (fn [details]
+                                (.warn js/console
+                                       (str "Failed to add hyphenation pattern: " details)))
+               :format :json)))
+
+(defonce app-state
+  (reagent/atom
+   {:hyphenations []}))
+
+(defn update-hyphenations! [f & args]
+  (apply swap! app-state update-in [:hyphenations] f args))
+
+(defn add-hyphenation! [h]
+  (update-hyphenations! conj h))
+
+(defn remove-hyphenation! [h]
+  (update-hyphenations! (fn [hs] (vec (remove #(= % h) hs))) h))
+
+(defn hyphenation-pattern [h]
+  [:tr
+   [:td (:word h)]
+   [:td (:hyphenation h)]
+   [:td
+    [:button.btn.btn-default
+     {:on-click #(remove-hyphenation! h)}
+     [:span.glyphicon.glyphicon-remove {:aria-hidden true}] " Delete"]]])
+
+(defn word-field [val]
+  [:div.form-group
+   [:label.sr-only {:for "wordInput"} "Word"]
+   [:input.form-control
+    {:id "wordInput"
+     :type "text"
+     :placeholder "Word"
+     :value @val
+     :on-change #(reset! val (-> % .-target .-value))}]])
+
+(defn hyphenation-field [val]
+  [:div.form-group
+   [:label.sr-only {:for "hyphenationInput"} "Hyphenation"]
+   [:input.form-control
+    {:type "text"
+     :placeholder "Hyphenation"
+     :value @val
+     :on-change #(reset! val (-> % .-target .-value))}]])
+
+(defn new-hyphenation []
+  (let [word (reagent/atom "")
+        hyphenation (reagent/atom "")]
+    (fn []
+      [:div.form-inline
+       [word-field word]
+       [hyphenation-field hyphenation]
+       [:button.btn.btn-default
+        {:on-click #(when (and @word @hyphenation)
+                      (add-hyphenation-pattern! app-state @word @hyphenation)
+                      (reset! word "")
+                      (reset! hyphenation ""))
+         :disabled (when (or (string/blank? @word)
+                             (string/blank? @hyphenation))
+                     "disabled")}
+        "Add"]])))
+
+(defn hyphenation-list []
+  [:div.container
+   [:h1 "Hyphenation list"]
+   [:table#hyphenations.table.table-striped
+    [:thead [:tr [:th "Word"] [:th "Hyphenation"]]]
+    [:tbody
+     (for [h (:hyphenations @app-state)]
+       ^{:key (:word h)} [hyphenation-pattern h])]]
+   [new-hyphenation]])
 
 ;; -------------------------
 ;; Views
 
 (defn home-page []
-  [:div [:h2 "Welcome to hyphen-keeper"]
-   [:div [:a {:href "/about"} "go to about page"]]])
+  [hyphenation-list])
 
 (defn about-page []
   [:div [:h2 "About hyphen-keeper"]
@@ -34,6 +124,7 @@
   (reagent/render [current-page] (.getElementById js/document "app")))
 
 (defn init! []
+  (load-hyphenation-patterns! app-state)
   (accountant/configure-navigation!
     {:nav-handler
      (fn [path]
