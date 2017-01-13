076d9b8497231b725112f43b86e9bbde6797196c
Christian Egli <christian.egli@sbs.ch>
2017-01-13 17:56:13 +0100

    Define the prod middleware

--- env/prod/clj/hyphen_keeper/middleware.clj
+++ env/prod/clj/hyphen_keeper/middleware.clj
@@ -1,5 +1,13 @@
 (ns hyphen-keeper.middleware
-  (:require [ring.middleware.defaults :refer [site-defaults wrap-defaults]]))
+  (:require [ring.middleware
+             [defaults :refer [api-defaults site-defaults wrap-defaults]]
+             [json :refer [wrap-json-params wrap-json-response]]]))
 
-(defn wrap-middleware [handler]
+(defn wrap-api-middleware [handler]
+  (-> handler
+      (wrap-defaults api-defaults)
+      wrap-json-params
+      wrap-json-response))
+
+(defn wrap-site-middleware [handler]
   (wrap-defaults handler site-defaults))
