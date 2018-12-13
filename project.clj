72f93d0e6fca0827975aa67a1d529c04f0df988d
Christian Egli <christian.egli@sbs.ch>
2018-12-13 15:18:24 +0100

    Fix project description and license info

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject hyphen-keeper "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A small web app to keep a whitelist of approved hyphenation patterns"
+  :url "https://github.com/sbsdev/hyphen-keeper"
+  :license {:name "GNU Affero General Public License"
+            :url "http://www.gnu.org/licenses/agpl-3.0.html"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.xml "0.1.0-beta3"]
